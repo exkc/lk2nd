e7b1526cb3001158f0aff7934fb9c2a87fbda66c
exkc <exkc@exkc.moe>
2025-02-04 03:01:21 +0800

    a

--- lk2nd/display/panel/generated/panels.h
+++ lk2nd/display/panel/generated/panels.h
@@ -7,6 +7,7 @@
 #include "lk_panel_ili9806e_boyi_byt45mp41t1m_4p5cpt.h"
 #include "lk_panel_ili9881c_hd_video.h"
 #include "lk_panel_r69431_720p_video.h"
+#include "lk_panel_auootm1287a_800p_video.h"
 
 int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 		     struct msm_panel_info *pinfo, struct mdss_dsi_phy_ctrl *phy_db);
