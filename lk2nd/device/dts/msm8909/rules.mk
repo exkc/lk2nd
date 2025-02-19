46a0e7d24bfc623545521de0a5df0f998109dac7
exkc <exkc@exkc.moe>
2025-02-19 19:13:37 +0800

    hack

--- lk2nd/device/dts/msm8909/rules.mk
+++ lk2nd/device/dts/msm8909/rules.mk
@@ -3,11 +3,4 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 
 QCDTBS += \
-	$(LOCAL_DIR)/msm8905-qrd-skub.dtb \
-	$(LOCAL_DIR)/msm8909-1gb-qrd-skuc.dtb \
-	$(LOCAL_DIR)/msm8909-mtp.dtb \
-	$(LOCAL_DIR)/msm8909-qrd-skue.dtb \
-	$(LOCAL_DIR)/msm8909-zte-sapphire.dtb \
-
-ADTBS += \
-        $(LOCAL_DIR)/apq8009w-wtp.dtb \
+	  $(LOCAL_DIR)/hack2.dtb \
